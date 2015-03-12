969509d3277602006afb5b09ea3fb04f6d2c77be
azu <azuciao@gmail.com>
2015-03-12 23:14:10 +0900

    fix(npm): fix require module name

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 module.exports = {
     Store: require("./lib/material-store"),
     Action: require("./lib/material-action"),
-    Context: require("./lib/material-flux")
+    Context: require("./lib/material-context")
 };
