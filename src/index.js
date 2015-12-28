00a6c45ac16fb19f945b5ba41a92ef6b0105e1ef
azu <azuciao@gmail.com>
2015-12-28 21:39:36 +0900

    docs(index): add comment to StoreGroup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 "use strict";
 module.exports = {
     Store: require("./material-store"),
+    // StoreGroup is collection of stores.
+    // StoreGroup has a feature that all stores are dispatched and does callback.
     StoreGroup: require("./material-store-group"),
     Action: require("./material-action"),
     Context: require("./material-context")
