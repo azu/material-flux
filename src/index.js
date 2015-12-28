adf98f3b4cd620e70c5a694d89ab028960501c44
azu <azuciao@gmail.com>
2015-12-28 21:29:58 +0900

    chore: separate container to other module

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ module.exports = {
     Store: require("./material-store"),
     StoreGroup: require("./material-store-group"),
     Action: require("./material-action"),
-    Context: require("./material-context"),
-    Container : require("./material-container")
+    Context: require("./material-context")
 };
